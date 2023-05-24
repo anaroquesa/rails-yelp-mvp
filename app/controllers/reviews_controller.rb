9ae831850bf3ece4b607f51c8bec5ddd38352faf
anasa <aroquesa@gmail.com>
2023-05-24 17:11:10 +0200

    restaurants without reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,6 +14,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     @review.save
     redirect_to @restaurant
   end
